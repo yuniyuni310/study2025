04701d24e119c1edfd952b14fbc5d3859b8c79ef
yuniyuni310 <166690688+yuniyuni310@users.noreply.github.com>
2025-07-16 14:28:22 +0900

    responsive

---
+++ responsive/javascript/js.js
@@ -0,0 +1,16 @@
+// 한줄주석
+// 제이쿼리선언
+
+// 화면을 열었을때,다음과같은 일들이 일어나라
+
+$(document).ready(function(){
+
+// 실행문작성
+//클래스toggle 을 클릭했을때, 클래스 gnb 가 슬라이드Toggle 되어라.
+
+$('.toggle').click(function(){
+
+$('.gnb').slideToggle()
+})
+
+})
